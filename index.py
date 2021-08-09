52ffefd6329441983a704f861de8df9bf7f3047a
daoquanganh <56275105+daoquanganh@users.noreply.github.com>
2021-08-09 21:19:20 +0300

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template,url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-app.run(host='localhost',port=5000)
